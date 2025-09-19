361ed1d27fbe1fd467265c1dfbd7abbf54cbb3ad
Clément DELCOURT <clm.delcourt@gmail.com>
2025-09-19 11:31:58 +0200

    Test

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/year', (req,res)=>{
-var aujd = new Date();
-console.log(aujd.now);
-var year = aujd.getFullYear();
+let year = new Date().getFullYear().toString();
 res.json({date : year});
 } 
 );
